77046906ac51a0b04b2ee674ac48759945d34e0f
Yotesh Mishra <yoteshmishra123@gmail.com>
2025-04-11 13:54:12 +0530

    changed in package.json and in the webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,20 +2,27 @@ const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin"); // Add this
 
 process.env["NODE_ENV"] = "production";
 
 module.exports = merge(common, {
   mode: "production",
   output: {
-    path: path.resolve(__dirname, "build"), // This is where build files will go
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
-    clean: true, // Optional: cleans the /build folder before build
+    clean: true,
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public/_redirects", to: "" } // Copy _redirects file
+      ],
+    }),
+  ],
   optimization: {
     minimize: true,
     minimizer: [
-      // You can also use ... here to include default JS minimizer
       `...`,
       new CssMinimizerPlugin(),
     ],
